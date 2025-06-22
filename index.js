7cd6b40ae33dbe1befb5d66ad7247a24381face6
Akshay Varma <varmaakshay2020@gmail.com>
2025-06-22 23:33:48 +0530

    Fix: allow CORS for deployed frontend

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",                    
+      "https://lone-town-client.vercel.app"       
+    ],
     methods: ["GET", "POST"]
   }
 });
+
+
 
 app.use(cors());
 
